cbc099b99374139a3643dfcd4c43ac5cf842cd48
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-01-28 18:48:22 +0100

    Aggiunge una classe Router

---
+++ Util/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Util;
+
+
+
+class Router {
+
+    private array $routes = [
+        'get' => [],
+        'post' => []
+    ];
+
+    function get($pattern, callable $handler) {
+        $this->routes['get'][$pattern] = $handler;
+        return $this;
+    }
+
+    function post($pattern, callable $handler) {
+        $this->routes['post'][$pattern] = $handler;
+        return $this;
+    }
+
+    function match(Request $request) : ?callable {
+        $method = strtolower($request->getMethod());
+        if (!isset($this->routes[$method])) {
+            return null;
+        }
+
+        $path = $request->getPath();
+        foreach ($this->routes[$method] as $pattern => $handler) {
+            if ($pattern === $path) {
+                return $handler;
+            }
+        }
+
+        return null;
+    }
+
+}
