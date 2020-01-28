26491b005ddfbaced01eb51965e43a9ba5f89b23
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-01-28 16:10:29 +0100

    Aggiunge della documentazione

--- Util/Request.php
+++ Util/Request.php
@@ -1,23 +1,39 @@
 <?php
 
-namespace Util;
+namespace Util {
 
-class Request {
+    /**
+     * Class Request
+     * @package Util
+     * Semplice classe che riceve i dati di una richiesta dal
+     * server e li rende disponibile in modo più chiaro
+     */
+    class Request {
+        private $method;
+        private $path;
 
-    private $method;
-    private $path;
+        /**
+         * Request constructor
+         * Recupera i dati dal server
+         */
+        function __construct() {
+            $this->method = $_SERVER['REQUEST_METHOD'];
+            $this->path = $_SERVER['REQUEST_URI'];
+        }
 
-    function __construct() {
-        $this->method = $_SERVER['REQUEST_METHOD'];
-        $this->path = $_SERVER['REQUEST_URI'];
+        /**
+         * @return string Una stringa che rappresenta un metodo (POST, GET, ...)
+         */
+        function getMethod() : string {
+            return $this->method;
+        }
+
+        /**
+         * @return string Ritorna il percorso della richiesta (ad esempio /post/list)
+         */
+        function getPath() : string{
+            return $this->path;
+        }
+
     }
-
-    function getMethod() {
-        return $this->method;
-    }
-
-    function getPath() {
-        return $this->path;
-    }
-
 }
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * Funzione per il caricamento automatico delle classi
+ * Ogni volta che nel codice viene creato un oggetto di una certa classe
+ * viene incluso il file che la riguarda:
+ * Es: se la classe fosse Util\Request verrà incluso il file Request che
+ * si trova nella cartella Util
+ */
+
 spl_autoload_register(function ($class_name) {
-    include_once __DIR__ . '/' . str_replace('\\','/',$class_name) . '.php';
+    include_once __DIR__ . '/' . str_replace('\\','/', $class_name) . '.php';
 });
 
 use Util\Request as Request;
 
+/**
+ * Esempio di creazione di una richiesta
+ * con la stampa del metodo e del path
+ */
 $request = new Request();
 echo '<p>Metodo della richiesta: ' . $request->getMethod() . '</p>';
 echo '<p>Percorso della richiesta: ' . $request->getPath() . '</p>';
