5537fc0abf187790781c7ac1a8485b58411df589
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-02-07 17:08:51 +0100

    Aggiunge la classe controller per le domande, con il metodo list

--- Controller/QuestionController.php
+++ Controller/QuestionController.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace Controller;
+
+use Model\QuestionRepository;
+
+class QuestionController{
+
+    private $template;
+
+    /**
+     * QuestionController constructor.
+     */
+    public function __construct($template)
+    {
+        $this->template = $template;
+    }
+
+    public function list()
+    {
+        $repo = new QuestionRepository();
+        $questions = $repo->getAllQuestions();
+        echo $this->template->render('questionList',['questions' => $questions]);
+    }
+
+}
